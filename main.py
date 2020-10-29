e25cdf405fbf0473cfb65d70a8a87d620755d9fb
Bob1437 <72892023+Bob1437@users.noreply.github.com>
2020-10-29 13:13:50 -0700

    second

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route("/home")
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route("/project")
+@app.route("/projects")
 def projects():
     return render_template("projects.html")
 
-@app.route("/journal")
-def journal():
+@app.route("/Ideas")
+def ideas():
     return render_template("Ideas.html")
 
 @app.route("/other")
@@ -20,4 +21,5 @@ def other():
     return render_template("other.html")
 
 if __name__ == "__main__":
+
     app.run(debug=True)
